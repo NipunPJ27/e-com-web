c55601481e2f152922a1ce72698c0f1cabc9653c
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-06-28 12:19:32 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>My next JS application</h1>
+    </main>
+  );
+}
